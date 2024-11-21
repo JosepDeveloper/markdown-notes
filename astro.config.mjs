fb9cb7af02a1d20263a2c9a73ee968b0dc2c8fa8
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-11-21 13:37:27 -0400

    Delete Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import preact from '@astrojs/preact';
-
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), svelte()]
+  integrations: [tailwind(), svelte()]
 });

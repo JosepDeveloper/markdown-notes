a23ce829b82e2e41b2b2e831f55abb6cbb46a254
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-11-27 16:35:56 -0400

    Add Vercel server

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import svelte from '@astrojs/svelte';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  output: 'server',
+  adapter: vercel()
 });

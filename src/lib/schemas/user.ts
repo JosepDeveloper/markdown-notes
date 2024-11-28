1cd5a06a7e1e625bda52063ae197f509541f95cb
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-11-28 15:17:26 -0400

    Add new schemas

--- src/lib/schemas/user.ts
+++ src/lib/schemas/user.ts
@@ -3,8 +3,18 @@ import {z} from 'zod'
 const userSchema = z.object({
   username: z.string().min(3).max(20),
   password: z.string().min(3).max(20),
-  securityQuestion: z.string().min(3),
-  responseSecurityQuestion: z.string().min(3).max(20)
+  recoveryCode: z.string().min(10)
 })
 
-export {userSchema}
+const loginUserSchema = z.object({
+  username: z.string().min(3).max(20),
+  password: z.string().min(3).max(20),
+})
+
+const recoverPasswordSchema = z.object({
+  username: z.string().min(3).max(20),
+  recoveryCode: z.string().min(10),
+  newPassword: z.string().min(3).max(20)
+})
+
+export {userSchema, loginUserSchema, recoverPasswordSchema}
